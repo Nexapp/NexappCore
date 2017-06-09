c4e619a772ce4e0a470e09e17793f52a555cce91
Alexandre Walsh <awalsh@nexapp.ca>
2017-06-09 16:47:57 -0400

    Added some useful methods in DateProvider

--- src/main/java/ca/nexapp/core/dates/DateProvider.java
+++ src/main/java/ca/nexapp/core/dates/DateProvider.java
@@ -1,8 +1,11 @@
 package ca.nexapp.core.dates;
 
+import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public interface DateProvider {
 
@@ -10,5 +13,15 @@ public interface DateProvider {
 
     LocalDateTime currentDateTime();
 
+    ZonedDateTime currentDateTime(ZoneId zone);
+
     LocalDate currentDate();
+
+    default DayOfWeek currentDayOfWeek() {
+        return currentDate().getDayOfWeek();
+    }
+
+    default DayOfWeek currentDayOfWeek(ZoneId zone) {
+        return currentDateTime(zone).getDayOfWeek();
+    }
 }
