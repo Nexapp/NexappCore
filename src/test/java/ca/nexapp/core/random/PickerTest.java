27279157e45fc6054110df0cddd53303495cc45b
Alexandre Walsh <awalsh@nexapp.ca>
2017-09-21 13:58:25 -0400

    Use the RepeatRule instead of a for each

--- src/test/java/ca/nexapp/core/random/PickerTest.java
+++ src/test/java/ca/nexapp/core/random/PickerTest.java
@@ -5,21 +5,23 @@ import static com.google.common.truth.Truth.assertThat;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 public class PickerTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @Repeat(100)
     public void shouldNeverPickAnIgnoredItem() {
-        // It's my best attempt to test randomness
-        for (int i = 0; i < 100; ++i) {
-            Picker<Integer> items = new Picker<>(Arrays.asList(1, 2, 3, 4, 5));
+        Picker<Integer> items = new Picker<>(Arrays.asList(1, 2, 3, 4, 5));
 
-            List<Integer> picked = items.pick(10, Arrays.asList(3, 4, 5));
+        List<Integer> picked = items.pick(10, Arrays.asList(3, 4, 5));
 
-            assertThat(picked).doesNotContain(3);
-            assertThat(picked).doesNotContain(4);
-            assertThat(picked).doesNotContain(5);
-        }
+        assertThat(picked).doesNotContain(3);
+        assertThat(picked).doesNotContain(4);
+        assertThat(picked).doesNotContain(5);
     }
 }
