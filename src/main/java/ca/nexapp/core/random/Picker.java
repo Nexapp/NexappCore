8d0b6021955d3fe730be11cf12907b510a21c2f6
Alexandre Walsh <awalsh@nexapp.ca>
2017-09-22 08:00:50 -0400

    Picker are now static

--- src/main/java/ca/nexapp/core/random/Picker.java
+++ src/main/java/ca/nexapp/core/random/Picker.java
@@ -2,30 +2,28 @@ package ca.nexapp.core.random;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-public class Picker<E> {
+public class Picker {
 
-    private final Collection<E> items;
-    private final Random random = new Random();
-
-    public Picker(Collection<E> items) {
-        this.items = items;
+    public static <E> Optional<E> pickOne(Collection<E> items) {
+        return pickOne(items, Collections.emptySet());
     }
 
-    public Optional<E> pickOne() {
-        return pick(1, new HashSet<>()).stream().findAny();
+    public static <E> Optional<E> pickOne(Collection<E> items, Collection<E> toIgnore) {
+        return pick(items, 1, toIgnore).stream().findAny();
     }
 
-    public List<E> pick(int limit) {
-        return pick(limit, new HashSet<>());
+    public static <E> List<E> pick(Collection<E> items, int limit) {
+        return pick(items, limit, Collections.emptySet());
     }
 
-    public List<E> pick(int limit, Collection<E> toIgnore) {
-        List<E> toPickFrom = new ArrayList<>(this.items);
+    public static <E> List<E> pick(Collection<E> items, int limit, Collection<E> toIgnore) {
+        Random random = new Random();
+        List<E> toPickFrom = new ArrayList<>(items);
         toPickFrom.removeAll(toIgnore);
 
         List<E> itemsFound = new ArrayList<>();
