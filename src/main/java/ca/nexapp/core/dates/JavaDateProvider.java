31a1bc42c6a9a1bbbc231f90220d0a215f45f2b9
Alexandre Walsh <awalsh@nexapp.ca>
2017-09-22 08:21:25 -0400

    Added a zoned current LocalDate, and removed all no-zone dates.

--- src/main/java/ca/nexapp/core/dates/JavaDateProvider.java
+++ src/main/java/ca/nexapp/core/dates/JavaDateProvider.java
@@ -1,32 +1,19 @@
 package ca.nexapp.core.dates;
 
-import java.time.Clock;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class JavaDateProvider implements DateProvider {
 
     @Override
-    public Instant currentTimestamp() {
-        return Instant.now(Clock.systemUTC());
-    }
-
-    @Override
-    public LocalDateTime currentDateTime() {
-        return LocalDateTime.now(Clock.systemUTC());
-    }
-
-    @Override
     public ZonedDateTime currentDateTime(ZoneId zone) {
-        return currentTimestamp().atZone(zone);
+        return ZonedDateTime.now(zone);
     }
 
     @Override
-    public LocalDate currentDate() {
-        return LocalDate.now(Clock.systemUTC());
+    public LocalDate currentDate(ZoneId zone) {
+        return LocalDate.now(zone);
     }
 
 }
